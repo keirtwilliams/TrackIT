fabc0535cb089fcfd06b9af39657becce3cef88b
KeirtWilliam Gemperle <keirtwilliamgemperle@gmail.com>
2025-08-29 13:18:20 +0800

    structuring

--- client/src/components/AppRow.jsx
+++ client/src/components/AppRow.jsx
@@ -1,17 +1,14 @@
-function AppRow({ company, position, status }){
-    return(
-        <div>
-          <table>
-            <thead>
-                <tr>
-                    <td>{company}</td>
-                    <td>{position}</td>
-                    <td>{status}</td>
-                    <td>Actions</td>
-                </tr>
-            </thead>
-          </table>
-        </div>
-    )
-};
+function AppRow({ company, position, status }) {
+  return (
+    <tr>
+      <td>{company}</td>
+      <td>{position}</td>
+      <td>{status}</td>
+      <td><button>Edit</button></td>
+      <td><button>Delete</button></td>
+
+    </tr>
+  );
+}
+
 export default AppRow;
--- client/src/components/AppTable.jsx
+++ client/src/components/AppTable.jsx
@@ -1,27 +1,37 @@
 import AppRow from "../components/AppRow";
 
+function AppTable({ onEdit, onDelete}) {
+  const newRoles = [
+    { id: 1, company: "JCorp", position: "Front-End Developer", status: "Pending" },
+    { id: 2, company: "ECorp", position: "Back-End Developer", status: "Interview" },
+    { id: 3, company: "GCorp", position: "FullStack Developer", status: "Rejected" },
+  ];
+    
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th>Position</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {newRoles.map((role) => (
+            <AppRow
+              key={role.id}
+              company={role.company}
+              position={role.position}
+              status={role.status}
+              onEdit={(e) => newRoles(e.target.value)}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
 
-function AppTable({ roles }){
-     const newRoles = [
-        { id: 1, company: "JCorp", position: "Front-End Developer"},
-        { id: 2, company: "ECorp", position: "Back-End Developer"},
-        { id: 3, company: "GCorp", position: "FullStack Developer"},
-     ];
-
-    return (
-        <div>
-            <table>
-           {roles.map((newRoles) => {
-             <AppRow 
-              key={newRoles.id}
-              company={newRoles.company}
-              position={newRoles.position}
-             />
-
-           })}
-           </table>
-        </div>
-        
-    )
-};
 export default AppTable;
